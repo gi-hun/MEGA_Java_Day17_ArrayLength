cb1ac3e9debf293f4df88350a37238630c1724f0
duarlgns1822@gmail.com <duarl@DESKTOP-RLI1BQT>
2020-03-11 23:35:05 +0900

    //배열 길이 구하기

---
+++ src/day_17/ArrayEx37.java
@@ -0,0 +1,12 @@
+package day_17;
+
+public class ArrayEx37 {
+	public static void main(String[] args) {
+		
+		int[] arr = new int[5];
+		
+		//배열 길이 구하기
+		int size = arr.length;
+		System.out.println("size = " + size);
+	}
+}
